adf2b37c9f75cde212f5b5c48a20e59c36599f99
Michelangelo Alesi <michelangelo.alesi.95@gmail.com>
2023-10-25 14:10:00 +0200

    feat(backend): :sparkles: add nodemailer support

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,4 +2,21 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_USERNAME'),
+        defaultReplyTo: env('SMTP_USERNAME'),
+      },
+    },
+  },
 });
