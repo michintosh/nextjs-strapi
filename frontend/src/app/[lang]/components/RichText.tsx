6bb4f12b97d8308231a74109fdcfc9e8e26358ce
Michelangelo Alesi <michelangelo.alesi.95@gmail.com>
2023-10-25 09:04:35 +0200

    add eslint

--- frontend/src/app/[lang]/components/RichText.tsx
+++ frontend/src/app/[lang]/components/RichText.tsx
@@ -1,5 +1,5 @@
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 interface RichTextProps {
   data: {
